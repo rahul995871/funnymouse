0b8c312ec7157eac5e13c0d890d321442496259d
riThvik <rahulsingh995871@gmail.com>
2024-01-17 12:52:14 +0530

    Add files via upload
    ff

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ $('.navbar .dropdown').hover(function() {
     $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideUp()
   });
 // navbar-end
+$(function() {
+  $('.pop').on('click', function() {
+    $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+    $('#imagemodal').modal('show');   
+  });		
+});
